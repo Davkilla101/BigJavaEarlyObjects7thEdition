59a9fad140cd676f7d6156f83305fbadf1fcd9ff
Davkilla101 <Cuetodavid95@gmail.com>
2022-07-15 14:04:44 -0400

    Added chapter 2, section 3's code

---
+++ chapters/ch02/sec03/ReplaceDemo.java
@@ -0,0 +1,8 @@
+public class ReplaceDemo
+{
+    public static void main(String[] args)
+    {
+        String river = "Mississippi";
+        System.out.println(river.replace("issipp", "our"));
+    }
+}
